35bce358d6eb74d6a5712fb19f35742f59037d7e
Oier592 <ogarciam23@fpsanturtzilh.eus>
2025-05-29 11:51:24 +0200

    Commit por si se va la luz

--- 1FinalaIkasleak/src/app/liburutegia/liburutegia.page.ts
+++ 1FinalaIkasleak/src/app/liburutegia/liburutegia.page.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { JsonIrakurriService } from '../zerbitzuak/json-irakurri.service';
+import { AlertController } from '@ionic/angular';
 interface ILiburua {
-  isbn:string;
-  titulo:string;
-  autor:string;
-  sinopsis:string;
-  imagen:string;
+  isbn: string;
+  titulo: string;
+  autor: string;
+  sinopsis: string;
+  imagen: string;
   argitaletxea: string;
   orrialdeak: number;
 }
-
 
 @Component({
   selector: 'app-liburutegia',
@@ -18,21 +17,57 @@ interface ILiburua {
   styleUrls: ['./liburutegia.page.scss'],
   standalone: false,
 })
-
-
 export class LiburutegiaPage implements OnInit {
+  liburuak: ILiburua[] = [];
 
-  
-
-  constructor() { }
+  constructor(
+    private jsonIrakurriService: JsonIrakurriService,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
     // irakurri liburuak zerbitzuari deitu
-
+    this.liburuak = this.jsonIrakurriService.get_Liburuak();
   }
 
   // FUNTZIOA 1: liburua gehitu alerta sortu
+  async Insert_Alerta() {
+    const alert = await this.alertController.create({
+      header: 'Liburu berria',
+      inputs: [
+        { name: 'titulua', placeholder: 'Titulua', type: 'text' },
+        { name: 'egilea', placeholder: 'Egilea', type: 'text' },
+        { name: 'isbn', placeholder: 'ISBN', type: 'text' },
+        { name: 'sinopsis', placeholder: 'Sinopsis', type: 'text' },
+        { name: 'irudia', placeholder: 'Irudia', type: 'text' },
+        { name: 'argitaletxea', placeholder: 'Argitaletxea', type: 'text' },
+        { name: 'orrialdeak', placeholder: 'Orrialdeak', type: 'number' },
 
- // FUNTZIOA 2: balidatu alertan sartu diren datuak
+      ],
+      buttons: [
+        {
+          text: 'Utzi',
+          role: 'cancel',
+        },
+        {
+          text: 'Gorde',
+          handler: (liburu_berria) => {
+            this.jsonIrakurriService.insert({
+              isbn: liburu_berria.isbn,
+              titulo: liburu_berria.titulua,
+              autor: liburu_berria.egilea,
+              sinopsis: liburu_berria.sinopsis,
+              imagen: liburu_berria.irudia,
+              argitaletxea: liburu_berria.argitaletxea,
+              orrialdeak: liburu_berria.orrialdeak
+            });
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
+
+  // FUNTZIOA 2: balidatu alertan sartu diren datuak
 }
--- 1FinalaIkasleak/src/app/zerbitzuak/json-irakurri.service.ts
+++ 1FinalaIkasleak/src/app/zerbitzuak/json-irakurri.service.ts
@@ -1,38 +1,59 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import json_liburuak from '../../assets/json/liburuak.json';
 
 interface ILiburua {
-  isbn:string;
-  titulo:string;
-  autor:string;
-  sinopsis:string;
-  imagen:string;
+  isbn: string;
+  titulo: string;
+  autor: string;
+  sinopsis: string;
+  imagen: string;
   argitaletxea: string;
   orrialdeak: number;
 }
 
 interface IJson {
-  isbn:string;
-  titulo:string;
-  autor:string;
-  sinopsis:string;
-  imagen:string;
+  isbn: string;
+  titulo: string;
+  autor: string;
+  sinopsis: string;
+  imagen: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
-
 export class JsonIrakurriService {
+  json_Fitxategia: IJson[] = [];
+  liburuak: ILiburua[] = [];
 
-  
-  constructor() { }
+  constructor() {
+    this.json_Kargatu();
+    this.liburutegi_Kargatu();
+  }
 
   // FUNTZIOA 1: liburuak JSON fitxategitik irakurtzen duen funtzioa eta falta diren datuak (argitaletxea eta orrialde kopurua)
   //balioak bueltatzen duen funtzioa
+  private json_Kargatu() {
+    this.json_Fitxategia = json_liburuak;
+  }
 
-  
+  private liburutegi_Kargatu() {
+    this.liburuak = json_liburuak.map((p) => ({
+      ...p,
+      argitaletxea: 'Ez da eurkitu',
+      orrialdeak: 404,
+    }));
+  }
+
+  public get_Liburuak(): ILiburua[] {
+    return this.liburuak;
+  }
+
+  insert(liburu_berria: ILiburua): void {
+    this.liburuak.push(liburu_berria);
+    //this.liburuak.total++;
+  }
 }
+// Es necesario el total de libros?
